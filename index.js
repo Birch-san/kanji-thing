47b1c6cd3339a067f4dd57698c3b1cf1d86df56e
Alex Birch <birch@Alexs-MacBook-Pro.local>
2015-08-26 22:46:24 +0100

    oh god JSON was a bad idea

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var _ = require('lodash');
 var util = require('util');
 var fs = require('fs');
+var lineReader = require('line-reader');
 
-var kanjiDicFilePath = './kanjidic.json';
+var kanjiDicFilePath = 'kanjidic.json';
 
 var kanjidicJsonFile;
 try {
@@ -18,21 +19,56 @@ try {
 	process.exit(1);
 }
 
-fs.readFile(kanjiDicFilePath,
-	{
-		encoding: 'utf8'
-	},
-	function (error, data) {
-		if (error) {
-			throw error;
-		}
+var henshallMax = 1450;
+
+var accumulator = [];
+
+function transform(entry, eNumber) {
+  return {
+    eNumber: eNumber,
+    kanji: entry.ki[0]
+  };
+}
+
+var sampleAll = true; 
+
+lineReader.eachLine(kanjiDicFilePath, function(line) {
+  var parsed = JSON.parse(line);
+  console.log(Object.keys(parsed));
+  console.log(parsed.gs);
+  var kanjiIndices = parsed.gs[0].split(" ");
+  console.log(kanjiIndices);
+  var eIndex = _.find(kanjiIndices, function(kanjiIndex) {
+    return kanjiIndex.match(/E(\d+)/);
+  });
+  var eNumber = +eIndex.match(/E(\d+)/)[1];
+  
+  if (eNumber <= henshallMax) {
+    accumulator.push(transform(parsed, eNumber));
+  }
+  
+  return sampleAll;
+})
+.then(function () {
+  console.log(accumulator);
+  process.exit(0);
+});
+
+// fs.readFile(kanjiDicFilePath,
+// 	{
+// 		encoding: 'utf8'
+// 	},
+// 	function (error, data) {
+// 		if (error) {
+// 			throw error;
+// 		}
 		
-		// split by line break into an array of strings
-		var split = subject.split(/\r?\n/);
+// 		// split by line break into an array of strings
+// 		var split = subject.split(/\r?\n/);
 		
-		// just grab the first one (for example)
-		var sample = split[0];
+// 		// just grab the first one (for example)
+// 		var sample = split[0];
 		
-		console.log(sample);
-		console.log(JSON.parse(sample));
-	});
+// 		console.log(sample);
+// 		console.log(JSON.parse(sample));
+// 	});
